413d1268816a493425611be06ce6568ead1f456e
google <rajugokul1996@gmail.com>
2020-09-15 22:53:41 +0530

    add_php_website

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://spaceraceit.com/wp/growth/home-3</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://spaceraceit.com/wp/growth/home-3/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://spaceraceit.com/wp/growth/home-3/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://spaceraceit.com/wp/growth/home-3/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://spaceraceit.com/wp/growth/home-3/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://spaceraceit.com/wp/growth/home-3/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
